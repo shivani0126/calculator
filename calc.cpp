37c4564e615606e3d771324fdf10833338bdcd44
Shivani <2604.shivanijoshi@gmail.com>
2021-10-02 14:37:41 +0530

    added multiply and division

--- calc.cpp
+++ calc.cpp
@@ -6,12 +6,19 @@ int add(int a, int b){
 int subtraction(int c,int d ){
     return c-d;
 }
-
+int multiply(int c,int d ){
+    return c*d;
+}
+int division(int c,int d ){
+    return c/d;
+ }
 int main() {
     int a,b,c,d;
     cout<<"enter the numbers:"<<endl;
     cin>>a>>b;
     cout<<add(a,b)<<endl;
     cout<<subtraction(a,b)<<endl;
+    cout<<multiply(a,b)<<endl;
+    cout<<division(a,b)<<endl;
     return 0;
 }
